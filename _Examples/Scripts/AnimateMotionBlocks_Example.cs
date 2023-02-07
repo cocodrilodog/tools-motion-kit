7aa29ca477bcb2dfa80c79686a7c8a2724b0e16a
OMEN\juanp <juanpablo.reyes@cocodrilodog.com>
2023-02-06 22:25:28 -0500

    Renamed `AnimateMotionBlocks_Example`

--- _Examples/Scripts/AnimateMotionBlocks_Example.cs
+++ _Examples/Scripts/AnimateMotionBlocks_Example.cs
@@ -5,7 +5,7 @@ namespace CocodriloDog.Animation.Examples {
 	using System.Collections.Generic;
 	using UnityEngine;
 
-	public class AnimateMotionComponents_Example : MonoBehaviour {
+	public class AnimateMotionBlocks_Example : MonoBehaviour {
 
 
 		#region Public Properties
